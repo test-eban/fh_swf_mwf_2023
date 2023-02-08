dfd5edfc5d852b578b218ecc84bf7fa47ef8d840
testeban <admin@testeban.de>
2023-02-08 22:58:29 +0100

    plpo

---
+++ mwf_wfm_frontend/src/components/buttons/testButton.vue
@@ -0,0 +1,15 @@
+<script>
+import { getAllUsers } from "../../services/UserService.js";
+
+export default {
+    data() {
+        return {
+            count: 0
+        }
+    }
+}
+</script>
+
+<template>
+  <button @click="getAllUsers()">You clicked me {{ count }} times.</button>
+</template>
