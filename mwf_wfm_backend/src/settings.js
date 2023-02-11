08e609a8f16cbe31ae89947a984cb18271dcd30b
testeban <admin@testeban.de>
2023-02-11 01:00:28 +0100

    [BACKEND] modify settings and package.json

--- mwf_wfm_backend/src/settings.js
+++ mwf_wfm_backend/src/settings.js
@@ -3,3 +3,8 @@ import dotenv from 'dotenv';
 dotenv.config();
 export const testEnvironmentVariable = process.env.TEST_ENV_VARIABLE;
 export const testVariable = process.env.TEST_ENV_VARIABLE2;
+
+export const dbHost = process.env.MARIADB_HOST;
+export const dbUser = process.env.MARIADB_USER;
+export const dbPassword = process.env.MARIADB_PASSWORD;
+export const dbDatabase = process.env.MARIADB_DB;
