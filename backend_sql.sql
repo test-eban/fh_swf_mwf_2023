a4869bdee23b709204ad54e4e790a1a7bac68a34
testeban <admin@testeban.de>
2023-02-13 22:14:58 +0100

    [BACKEND] refine sql statements for backend tables

--- backend_sql.sql
+++ backend_sql.sql
@@ -8,47 +8,61 @@ CREATE TABLE IF NOT EXISTS User (
   updatedBy VARCHAR(64) -- -> User
 );
 
-ALTER TABLE User ADD COLUMN createdBy USER, updatedBy USER;
-
 CREATE TABLE IF NOT EXISTS Branch (
 	name VARCHAR(128) PRIMARY KEY,
 	description VARCHAR(256),
 	achieved BOOLEAN,
 	createdOn DATETIME,
-	createdBy VARCHAR(64), -- -> User
+	createdBy VARCHAR(64) not null, -- -> User
 	updatedOn DATETIME,
-	updatedBy VARCHAR(64) -- -> User
+	updatedBy VARCHAR(64) not null -- -> User
 );
 
 CREATE TABLE IF NOT EXISTS TaskType (
 	name VARCHAR(128) PRIMARY KEY,
 	description VARCHAR(256),
-	priority priorityEnum,
+	priority ENUM('low', 'medium', 'high', 'critical'),
 	achieved BOOLEAN,
 	createdOn DATETIME,
-	createdBy VARCHAR(64), -- -> User
+	createdBy VARCHAR(64) not null, -- -> User
 	updatedOn DATETIME,
-	updatedBy VARCHAR(64) -- -> User
+	updatedBy VARCHAR(64) not null -- -> User
 );
 
 CREATE TABLE IF NOT EXISTS Task (
 	uid SERIAL PRIMARY KEY,
-	type VARCHAR(128), -- -> Type
-	branch VARCHAR(128), -- -> Branch
+	taskType VARCHAR(128) not null, -- -> Type
+	branch VARCHAR(128) not null, -- -> Branch
 	label VARCHAR(256),
-	description VARCHAR(-1),
+	description TEXT,
 	startDate DATETIME,
 	endDate DATETIME,
-	status statusEnum
+	status ENUM('created', 'planned', 'started', 'finished', 'aborted'),
 	abortingRemark VARCHAR(256),
 	finishingRemark VARCHAR(256),
 	createdOn DATETIME,
-	createdBy VARCHAR(64), -- -> User
+	createdBy VARCHAR(64) not null, -- -> User
 	updatedOn DATETIME,
-	updatedBy VARCHAR(64), -- -> User
+	updatedBy VARCHAR(64) not null, -- -> User
 	startedOn DATETIME,
 	startedBy VARCHAR(64), -- -> User
 	finishedOn DATETIME,
 	finishedBy VARCHAR(64), -- -> User
 	assignedTo VARCHAR(64) -- -> User
 );
+
+ALTER TABLE TaskType ADD CONSTRAINT FK_TaskType_createdBy_login_User FOREIGN KEY (createdBy) REFERENCES User (login) ON UPDATE CASCADE ON DELETE NO ACTION;
+ALTER TABLE TaskType ADD CONSTRAINT FK_TaskType_updatedBy_login_User FOREIGN KEY (updatedBy) REFERENCES User (login) ON UPDATE CASCADE ON DELETE NO ACTION;
+
+ALTER TABLE Branch ADD CONSTRAINT FK_Branch_createdBy_login_User FOREIGN KEY (createdBy) REFERENCES User (login) ON UPDATE CASCADE ON DELETE NO ACTION;
+ALTER TABLE Branch ADD CONSTRAINT FK_Branch_updatedBy_login_User FOREIGN KEY (updatedBy) REFERENCES User (login) ON UPDATE CASCADE ON DELETE NO ACTION;
+
+ALTER TABLE Task ADD CONSTRAINT FK_Task_createdBy_login_User FOREIGN KEY (createdBy) REFERENCES User (login) ON UPDATE CASCADE ON DELETE NO ACTION;
+ALTER TABLE Task ADD CONSTRAINT FK_Task_updatedBy_login_User FOREIGN KEY (updatedBy) REFERENCES User (login) ON UPDATE CASCADE ON DELETE NO ACTION;
+ALTER TABLE Task ADD CONSTRAINT FK_Task_startedBy_login_User FOREIGN KEY (startedBy) REFERENCES User (login) ON UPDATE CASCADE ON DELETE NO ACTION;
+ALTER TABLE Task ADD CONSTRAINT FK_Task_finishedBy_login_ground_zeroUser FOREIGN KEY (finishedBy) REFERENCES User (login) ON UPDATE CASCADE ON DELETE NO ACTION;
+ALTER TABLE Task ADD CONSTRAINT FK_Task_assignedTo_login_User FOREIGN KEY (assignedTo) REFERENCES User (login) ON UPDATE CASCADE ON DELETE NO ACTION;
+
+ALTER TABLE Task ADD CONSTRAINT FK_Task_taskType_name_TaskType FOREIGN KEY (taskType) REFERENCES TaskType (name) ON UPDATE CASCADE ON DELETE NO ACTION;
+
+ALTER TABLE Task ADD CONSTRAINT FK_Task_taskType_name_Branch FOREIGN KEY (taskType) REFERENCES Branch (name) ON UPDATE CASCADE ON DELETE NO ACTION;
