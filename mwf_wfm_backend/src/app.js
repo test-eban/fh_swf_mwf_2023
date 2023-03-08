605a27f7cb6630f0f834a1b66645d9edafafdcc7
testeban <admin@testeban.de>
2023-03-09 00:41:23 +0100

    [BACKEND] add cors to backend

--- mwf_wfm_backend/src/app.js
+++ mwf_wfm_backend/src/app.js
@@ -2,10 +2,18 @@ import logger from 'morgan';
 import express from 'express';
 import cookieParser from 'cookie-parser';
 import indexRouter from './routes/index.js';
+import cors from 'cors';
 
 const app = express();
 
+var corsOptions = {
+    origin: 'http://localhost:8080',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+// TODO korrekt einbinden
+
 app.use(logger('dev'));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
