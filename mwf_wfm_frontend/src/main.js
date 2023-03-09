50b86f563017eaca82ac824c9483c694788a3b95
testeban <admin@testeban.de>
2023-03-09 20:32:37 +0100

    [FRONTEND] add bootstrap-next

--- mwf_wfm_frontend/src/main.js
+++ mwf_wfm_frontend/src/main.js
@@ -2,5 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import 'bulma/css/bulma.css';
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue-next/dist/bootstrap-vue-next.css'
 
 createApp(App).use(router).mount('#app')
